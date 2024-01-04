2a434f42e127bc36bfb68f79a49d986d1204922d
Marjane Oliveira <marjane_rodrigues@hotmail.com>
2024-01-04 18:10:52 +0100

    project Criptomonedas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
-import ImagenCripto from "./img/imagen-criptos.png";
 import Formulario from "./components/Formulario";
+import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
+import ImagenCripto from "./img/imagen-criptos.png";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -40,12 +43,37 @@ const Heading = styled.h1`
 `;
 
 function App() {
+  const [monedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(monedas).length > 0) {
+      const cotizarCripto = async () => {
+        setCargando(true);
+        setResultado({});
+        const { moneda, criptomoneda } = monedas;
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        setResultado(resultado.DISPLAY[criptomoneda][moneda]);
+        setCargando(false);
+      };
+      cotizarCripto();
+    }
+  }, [monedas]);
+
   return (
     <Contenedor>
       <Imagen src={ImagenCripto} alt="imagenes criptomonedas" />
       <div>
         <Heading>Cotizador de Criptomonedas</Heading>
-        <Formulario />
+        <Formulario setMonedas={setMonedas} />
+        {cargando && <Spinner />}
+
+        {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
   );
